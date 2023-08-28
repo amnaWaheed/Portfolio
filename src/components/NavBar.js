a956d7af62c543fdc2eb42adc04ea424d1457474
Amna Waheed <amnawaheed445@gmail.com>
2023-08-28 11:16:11 -0700

    worked on logo and framer motion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,19 @@
  import Link from 'next/link'
 import React from 'react'
- 
+import Logo from './Logo'
+
  const NavBar = () => {
    return (
-     <header className='w-full px-32 py-8 font-medium flex items-center justify-content-between' >
+     <header className='w-full px-32 py-8 font-medium flex items-center justify-between' >
         <nav>
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/projects">Projects</Link>
           <Link href="/articles">Articles</Link>
         </nav>
-        <h2>logo</h2>
+        <h2>
+          <Logo/>
+        </h2>
         <nav>
             <Link href="/" target={"_blank"}>T</Link>
             <Link href="/" target={"_blank"}>T</Link>
