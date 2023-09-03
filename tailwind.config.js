773777603821249107c3dab7aefe4d2129e6f883
Amna Waheed <amnawaheed445@gmail.com>
2023-09-03 11:11:16 -0700

    worked on navbar and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const {fontFamily} = require("tailwindcss/defaultTheme")
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
  
